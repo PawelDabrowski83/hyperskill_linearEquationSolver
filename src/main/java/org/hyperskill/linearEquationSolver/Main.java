43fcac65f35ba0022a50de053432dd90d7c673b2
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-05-25 08:51:37 +0200

    stage 1 complete and tested

--- src/main/java/org/hyperskill/linearEquationSolver/Main.java
+++ src/main/java/org/hyperskill/linearEquationSolver/Main.java
@@ -1,4 +1,38 @@
 package org.hyperskill.linearEquationSolver;
 
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Course is giving me input via Scanner, I am passing data to external methods for testing
+         */
+
+        // equation a * x = b
+        double a = 0;
+        double b = 0;
+        try (Scanner scanner = new Scanner(System.in)) {
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("incorrect input");
+        }
+        try {
+            double result = solveLinear(a, b);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public static double solveLinear (double a, double b) throws ArithmeticException {
+        // equation a * x = b
+        if (a == 0) {
+            throw new ArithmeticException("Cannot divide by zero!");
+        }
+        return b / a;
+    }
 }
--- src/test/java/org/hyperskill/linearEquationSolver/MainTest.java
+++ src/test/java/org/hyperskill/linearEquationSolver/MainTest.java
@@ -1,7 +1,24 @@
 package org.hyperskill.linearEquationSolver;
 
-public class MainTest {
-    public static void main(String[] args) {
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void shouldSolveLinearWork() {
+        assertEquals(0.6, Main.solveLinear(5, 3), 0.1);
+        assertEquals(0.5, Main.solveLinear(8, 4), 0.1);
+        assertEquals(0, Main.solveLinear(22, 0), 0.1);
     }
+
+    @Test (expected = ArithmeticException.class)
+    public void shouldGivenDividerZeroThrowArithmeticException() {
+        // when
+        double actual = Main.solveLinear(0, 17);
+        // then
+        // throws exception
+    }
+
 }
