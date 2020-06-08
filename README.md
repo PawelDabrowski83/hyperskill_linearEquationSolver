474436400d387cb3fde0c8e51a5c2c191b6c6dc3
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-08 16:38:04 +0200

    readme update - roadplan

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # hyperskill_linearEquationSolver
 
 Simple arithmetic practice, fun with GitHub branches.
+
+Stages:
+* Fraction representation
+* Fraction multiplication
+* Fraction add
+* Fraction reduce and set minut to nominator (upper part)
+* Equation representation
+* Equation adding and multiplying
+* Equation find leadingEntry and leadingEntryColumn
+* Equation make leadingEntry value 1
+* Matrix representation
+* Matrix sort by Equation length
+* Reduce Equation to have only 1 with given length
+
--- src/main/java/org/hyperskill/linearEquationSolver/Main.java
+++ src/main/java/org/hyperskill/linearEquationSolver/Main.java
@@ -11,17 +11,26 @@ public class Main {
         Course is giving me input via Scanner, I am passing data to external methods for testing
          */
 
-        // equation a * x = b
-        double a = 0;
-        double b = 0;
+        // { a * x + b * y = c
+        // { d * x + e * y = f
+        double aVar = 0;
+        double bVar = 0;
+        double cVar = 0;
+        double dVar = 0;
+        double eVar = 0;
+        double fVar = 0;
         try (Scanner scanner = new Scanner(System.in)) {
-            a = scanner.nextDouble();
-            b = scanner.nextDouble();
+            aVar = scanner.nextDouble();
+            bVar = scanner.nextDouble();
+            cVar = scanner.nextDouble();
+            dVar = scanner.nextDouble();
+            eVar = scanner.nextDouble();
+            fVar = scanner.nextDouble();
         } catch (InputMismatchException e) {
             System.out.println("incorrect input");
         }
         try {
-            double result = solveLinear(a, b);
+            double result = solveLinear(aVar, bVar);
             System.out.println(result);
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage());
@@ -34,5 +43,16 @@ public class Main {
             throw new ArithmeticException("Cannot divide by zero!");
         }
         return b / a;
+    }
+
+    public static double[] solveTwoSimultaneousLinears(double aVar, double bVar, double cVar, double dVar, double eVar
+            , double fVar) {
+        // { a * x + b * y = c
+        // { d * x + e * y = f
+
+        double y = (fVar - cVar * dVar / aVar) / (eVar - bVar * dVar / aVar);
+        double x = (cVar - bVar * y) / aVar;
+        return new double[]{x, y};
+
     }
 }
