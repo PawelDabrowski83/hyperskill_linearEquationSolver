c543c1afe4397eaf9991c6f7d6a70cb723e7a3cf
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-09 13:21:39 +0200

    fractions - test cases for greatestCommonDivisor

---
+++ src/main/java/org/hyperskill/linearEquationSolver/Fraction.java
@@ -0,0 +1,35 @@
+package org.hyperskill.linearEquationSolver;
+
+public class Fraction {
+
+    final int numerator;
+    final int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Fraction)) return false;
+
+        Fraction fraction = (Fraction) o;
+
+        if (numerator != fraction.numerator) return false;
+        return denominator == fraction.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = numerator;
+        result = 31 * result + denominator;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Fraction{" + numerator + "/" + denominator + "}";
+    }
+}
---
+++ src/test/java/org/hyperskill/linearEquationSolver/MathUtilsTest.java
@@ -0,0 +1,92 @@
+package org.hyperskill.linearEquationSolver;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MathUtilsTest {
+
+    @Test
+    public void shouldFindGCDGiven4And2Return2() {
+        // given
+        int a = 4;
+        int b = 2;
+        int expected = 2;
+
+        // when
+        int actual = MathUtils.findGCD(a, b);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindGCDGiven3And7Return21() {
+        // given
+        int a = 3;
+        int b = 7;
+        int expected = 1;
+
+        // when
+        int actual = MathUtils.findGCD(a, b);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindGCDGiven4And2Return2() {
+        // given
+        int a = 4;
+        int b = 2;
+        int expected = 2;
+
+        // when
+        int actual = MathUtils.findGCD(a, b);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindGCDGiven10And2Return2() {
+        // given
+        int a = 10;
+        int b = 2;
+        int expected = 2;
+
+        // when
+        int actual = MathUtils.findGCD(a, b);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindGCDGiven8AndNeg2Return2() {
+        // given
+        int a = 8;
+        int b = -2;
+        int expected = 2;
+
+        // when
+        int actual = MathUtils.findGCD(a, b);
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindGCDGiven0AndNeg2Return0() {
+        // given
+        int a = 0;
+        int b = -2;
+        int expected = 0;
+
+        // when
+        int actual = MathUtils.findGCD(a, b);
+
+        // then
+        assertEquals(expected, actual);
+    }
+}
