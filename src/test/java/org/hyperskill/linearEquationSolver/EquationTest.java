02cf95d2082d9e8a7d569ecc41d27baf3b50eff0
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-11 12:45:02 +0200

    Equation - tests for findLeadingEntry and findLeadingPosition

--- src/test/java/org/hyperskill/linearEquationSolver/EquationTest.java
+++ src/test/java/org/hyperskill/linearEquationSolver/EquationTest.java
@@ -84,4 +84,169 @@ public class EquationTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldFindLeadingEntryGiven1_1And0_1And4_5AndNeg8_1Returns1_1() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ONE, Fraction.ZERO, new Fraction(4, 5), new Fraction(-8, 1)});
+        Fraction expected = Fraction.ONE;
+
+        // when
+        Fraction actual = equation.findLeadingEntry();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingEntryGiven0_1And0_1And4_5AndNeg8_1Returns4_5() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ZERO, new Fraction(4, 5), new Fraction(-8, 1)});
+        Fraction expected = new Fraction(4, 5);
+
+        // when
+        Fraction actual = equation.findLeadingEntry();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingEntryGiven0_1And0_1And0_1AndNeg8_1ReturnsNeg8_1() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, new Fraction(-8, 1)});
+        Fraction expected = new Fraction(-8, 1);
+
+        // when
+        Fraction actual = equation.findLeadingEntry();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingEntryGiven0_1And0_1And0_1And0_1Returns0_1() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO});
+        Fraction expected = Fraction.ZERO;
+
+        // when
+        Fraction actual = equation.findLeadingEntry();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven2_3And1_1And0_1And9_2AndNeg1_1Returns0() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                new Fraction(2, 3), Fraction.ONE, Fraction.ZERO, new Fraction(9, 2), Fraction.NEG_ONE});
+        int expected = 0;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven0_1And1_1And0_1And9_2AndNeg1_1Returns1() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ONE, Fraction.ZERO, new Fraction(9, 2), Fraction.NEG_ONE});
+        int expected = 1;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven0_1And0_1And0_1And9_2AndNeg1_1Returns3() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, new Fraction(9, 2), Fraction.NEG_ONE});
+        int expected = 3;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven0_1And0_1And0_1And0_1AndNeg1_1Returns4() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.NEG_ONE});
+        int expected = 4;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven0_1And0_1And0_1And0_1And0_1ReturnsNeg1() {
+        // given
+        Equation equation = new Equation(new Fraction[]{
+                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO});
+        int expected = -1;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGivenEmptyReturnsNeg1() {
+        // given
+        Equation equation = new Equation(new Fraction[0]);
+        int expected = -1;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven0_1ReturnsNeg1() {
+        // given
+        Equation equation = new Equation(new Fraction[]{Fraction.ZERO});
+        int expected = -1;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindLeadingPositionGiven1_1Returns0() {
+        // given
+        Equation equation = new Equation(new Fraction[]{Fraction.ONE});
+        int expected = 0;
+
+        // when
+        int actual = equation.findLeadingPosition();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
 }
