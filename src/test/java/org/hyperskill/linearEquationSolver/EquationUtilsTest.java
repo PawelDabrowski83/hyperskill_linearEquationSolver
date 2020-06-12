792a77362cf6068fb910b32e910fcb85f1101ad2
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-12 15:19:02 +0200

    EquationUtils - tests refactored with parametrized tests in progress

--- src/test/java/org/hyperskill/linearEquationSolver/EquationUtilsTest.java
+++ src/test/java/org/hyperskill/linearEquationSolver/EquationUtilsTest.java
@@ -1,71 +1,62 @@
 package org.hyperskill.linearEquationSolver;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
+@DisplayName("Should EquationUtils - external methods for Equation class")
 public class EquationUtilsTest {
 
-    Equation equation;
-    Equation equation1;
-    Equation equation2;
-    Equation expected;
-    Equation actual;
-
-    @Test
-    public void shouldAddEquationGiven1_2AndNeg3_6And1_7Plus1_4And9_2AndNeg8_3Returns() {
-        // given
-        equation1 = new Equation(new Fraction[]{new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)});
-        equation2 = new Equation(new Fraction[]{new Fraction(1, 4), new Fraction(9, 2), new Fraction(-8, 3)});
-        expected = new Equation(new Fraction[]{new Fraction(3, 4), new Fraction(4, 1), new Fraction(-53, 21)});
-
-        // when
-        actual = EquationUtils.addEquation(equation1, equation2);
-
-        // then
-        assertEquals(expected, actual);
+    @DisplayName("Should addEquation work")
+    @ParameterizedTest(name = "{index} => expected={0}, equation1={1}, equation2={2}")
+    @ArgumentsSource(AddEquationArgumentsProvider.class)
+    void addEquation(Equation expected, Equation equation1, Equation equation2) {
+        assertEquals(expected, EquationUtils.addEquation(equation1, equation2));
     }
 
-    @Test
-    public void shouldAddEquationGiven1_2AndNeg3_6And1_7Plus9_2AndNeg8_3Returns() {
-        // given
-        equation1 = new Equation(new Fraction[]{new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)});
-        equation2 = new Equation(new Fraction[]{new Fraction(9, 2), new Fraction(-8, 3)});
-        expected = new Equation(new Fraction[]{new Fraction(5, 1), new Fraction(-19, 6), new Fraction(1, 7)});
+    static class AddEquationArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+            return Stream.of(
+                    Arguments.of(   new Equation(new Fraction[]{
+                                            new Fraction(3, 4), new Fraction(4, 1), new Fraction(-53, 21)}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(1, 4), new Fraction(9, 2), new Fraction(-8, 3)})
+                    ),
+                    Arguments.of(   new Equation(new Fraction[]{
+                                            new Fraction(5, 1), new Fraction(-19, 6), new Fraction(1, 7)}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(9, 2), new Fraction(-8, 3)})
 
-        // when
-        actual = EquationUtils.addEquation(equation1, equation2);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldAddEquationGiven1_7AndNeg3_2AndNeg1_1Plus1_4AndNeg2_1And3_4Returns() {
-        // given
-        equation1 = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-        equation2 = new Equation(new Fraction[]{new Fraction(1, 4), new Fraction(-2, 1), new Fraction(3, 4)});
-        expected = new Equation(new Fraction[]{new Fraction(11, 28), new Fraction(-7, 2), new Fraction(-1, 4)});
-
-        // when
-        actual = EquationUtils.addEquation(equation1, equation2);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldAddEquationGiven1_7AndNeg3_2AndNeg1_1Plus0_1AndNeg2_1And0_1Returns() {
-        // given
-        equation1 = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-        equation2 = new Equation(new Fraction[]{Fraction.ZERO, new Fraction(-2, 1), Fraction.ZERO});
-        expected = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-7, 2), Fraction.NEG_ONE});
-
-        // when
-        actual = EquationUtils.addEquation(equation1, equation2);
-
-        // then
-        assertEquals(expected, actual);
+                    ),
+                    Arguments.of(   new Equation(new Fraction[]{
+                                            new Fraction(11, 28), new Fraction(-7, 2), new Fraction(-1, 4)}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(1, 4), new Fraction(-2, 1), new Fraction(3, 4)})
+                    ),
+                    Arguments.of(   new Equation(new Fraction[]{
+                                            new Fraction(1, 7), new Fraction(-7, 2), Fraction.NEG_ONE}),
+                                    new Equation(new Fraction[]{
+                                            new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE}),
+                                    new Equation(new Fraction[]{
+                                            Fraction.ZERO, new Fraction(-2, 1), Fraction.ZERO})
+                    )
+            );
+        }
     }
 
     @Test
