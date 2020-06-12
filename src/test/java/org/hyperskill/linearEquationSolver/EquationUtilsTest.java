f371168568a34e82d729b9af3eda0a7db684cfa3
Paweł Dąbrowski <48104564+PawelDabrowski83@users.noreply.github.com>
2020-06-12 15:54:02 +0200

    EquationUtils - tests refactored for parametered tests

--- src/test/java/org/hyperskill/linearEquationSolver/EquationUtilsTest.java
+++ src/test/java/org/hyperskill/linearEquationSolver/EquationUtilsTest.java
@@ -1,12 +1,9 @@
 package org.hyperskill.linearEquationSolver;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsProvider;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
@@ -17,235 +14,181 @@ public class EquationUtilsTest {
 
     @DisplayName("Should addEquation work")
     @ParameterizedTest(name = "{index} => expected={0}, equation1={1}, equation2={2}")
-    @ArgumentsSource(AddEquationArgumentsProvider.class)
+    @MethodSource("addEquationArgumentsProvider")
     void addEquation(Equation expected, Equation equation1, Equation equation2) {
         assertEquals(expected, EquationUtils.addEquation(equation1, equation2));
     }
 
-    static class AddEquationArgumentsProvider implements ArgumentsProvider {
-        @Override
-        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
-            return Stream.of(
-                    Arguments.of(   new Equation(new Fraction[]{
-                                            new Fraction(3, 4), new Fraction(4, 1), new Fraction(-53, 21)}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(1, 4), new Fraction(9, 2), new Fraction(-8, 3)})
-                    ),
-                    Arguments.of(   new Equation(new Fraction[]{
-                                            new Fraction(5, 1), new Fraction(-19, 6), new Fraction(1, 7)}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(9, 2), new Fraction(-8, 3)})
+    private static Stream<Arguments> addEquationArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(3, 4), new Fraction(4, 1), new Fraction(-53, 21)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 4), new Fraction(9, 2), new Fraction(-8, 3)
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(5, 1), new Fraction(-19, 6), new Fraction(1, 7)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 2), new Fraction(-3, 6), new Fraction(1, 7)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(9, 2), new Fraction(-8, 3)
+                        })
 
-                    ),
-                    Arguments.of(   new Equation(new Fraction[]{
-                                            new Fraction(11, 28), new Fraction(-7, 2), new Fraction(-1, 4)}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(1, 4), new Fraction(-2, 1), new Fraction(3, 4)})
-                    ),
-                    Arguments.of(   new Equation(new Fraction[]{
-                                            new Fraction(1, 7), new Fraction(-7, 2), Fraction.NEG_ONE}),
-                                    new Equation(new Fraction[]{
-                                            new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE}),
-                                    new Equation(new Fraction[]{
-                                            Fraction.ZERO, new Fraction(-2, 1), Fraction.ZERO})
-                    )
-            );
-        }
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(11, 28), new Fraction(-7, 2), new Fraction(-1, 4)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 4), new Fraction(-2, 1), new Fraction(3, 4)
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-7, 2), Fraction.NEG_ONE
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, new Fraction(-2, 1), Fraction.ZERO
+                        })
+                )
+        );
     }
 
-    @Test
-    public void shouldMultiplyEquationGiven1_7AndNeg3_2AndNeg1_1Times1_1Returns() {
-        // given
-        equation = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-        Fraction multiplier = Fraction.ONE;
-        expected = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-
-        // when
-        actual = EquationUtils.multiplyEquation(equation, multiplier);
-
-        // then
-        assertEquals(expected, actual);
+    @DisplayName("Should multiplyEquation work")
+    @ParameterizedTest(name = "{index} => expected={0}, equation={1}, multiplier={2}")
+    @MethodSource("multiplyEquationArgumentsProvider")
+    void multiplyEquation(Equation expected, Equation equation, Fraction multiplier) {
+        assertEquals(expected, EquationUtils.multiplyEquation(equation, multiplier));
+    }
+    private static Stream<Arguments> multiplyEquationArgumentsProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        Fraction.ONE
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        Fraction.ZERO
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(-2, 21), Fraction.ONE, new Fraction(2, 3)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        new Fraction(-2, 3)
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(5, 28), new Fraction(-15, 8), new Fraction(-5, 4)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE
+                        }),
+                        new Fraction(5, 4)
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                new Fraction(2, 7), new Fraction(-3, 1), Fraction.ZERO
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(1, 7), new Fraction(-3, 2), Fraction.ZERO
+                        }),
+                        new Fraction(2, 1)
+                )
+        );
     }
 
-    @Test
-    public void shouldMultiplyEquationGiven1_7AndNeg3_2AndNeg1_1Times0_1Returns() {
-        // given
-
-        equation = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-        Fraction multiplier = Fraction.ZERO;
-        expected = new Equation(new Fraction[]{Fraction.ZERO, Fraction.ZERO, Fraction.ZERO});
-
-        // when
-        actual = EquationUtils.multiplyEquation(equation, multiplier);
-
-        // then
-        assertEquals(expected, actual);
+    @DisplayName("Should makeLeadingOne transfer equation that leading entry is one")
+    @ParameterizedTest(name = "{index} => expected={0}, actual={1}")
+    @MethodSource("makeLeadingOneArgumentsProvider")
+    void makeLeadingOne(Equation expected, Equation equation) {
+        assertEquals(expected, EquationUtils.makeLeadingOne(equation));
     }
-
-    @Test
-    public void shouldMultiplyEquationGiven1_7AndNeg3_2AndNeg1_1TimesNeg2_3Returns() {
-        // given
-        equation = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-        Fraction multiplier = new Fraction(-2, 3);
-        expected = new Equation(new Fraction[]{new Fraction(-2, 21), Fraction.ONE, new Fraction(2, 3)});
-
-        // when
-        actual = EquationUtils.multiplyEquation(equation, multiplier);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMultiplyEquationGiven1_7AndNeg3_2AndNeg1_1Times5_4Returns() {
-        // given
-        equation = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.NEG_ONE});
-        Fraction multiplier = new Fraction(5, 4);
-        expected = new Equation(new Fraction[]{new Fraction(5, 28), new Fraction(-15, 8), new Fraction(-5, 4)});
-
-        // when
-        actual = EquationUtils.multiplyEquation(equation, multiplier);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMultiplyEquationGiven1_7AndNeg3_2And0_1Times2_1Returns() {
-        // given
-        equation = new Equation(new Fraction[]{new Fraction(1, 7), new Fraction(-3, 2), Fraction.ZERO});
-        Fraction multiplier = new Fraction(2, 1);
-        expected = new Equation(new Fraction[]{new Fraction(2, 7), new Fraction(-3, 1), Fraction.ZERO});
-
-        // when
-        actual = EquationUtils.multiplyEquation(equation, multiplier);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven2_3And3_4AndNeg1_7And0_1And5_6Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                new Fraction(2, 3), new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ONE, new Fraction(9, 8), new Fraction(-3, 14), Fraction.ZERO, new Fraction(5, 4)
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven0_1And3_4AndNeg1_7And0_1And5_6Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                Fraction.ZERO, new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ONE, new Fraction(-4, 21), Fraction.ZERO, new Fraction(10, 9)
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven0_1And0_1AndNeg1_7And0_1And5_6Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ONE, Fraction.ZERO, new Fraction(-35, 6)
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven0_1And0_1And0_1And0_1And5_6Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, new Fraction(5, 6)
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ONE
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven0_1And0_1And0_1And0_1And0_1Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven0_1And0_1And0_1And0_1And1_1Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ONE
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ONE
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldMakeLeadingOneGiven1_1And3_4AndNeg1_7And0_1And5_6Returns() {
-        // given
-        equation = new Equation(new Fraction[]{
-                Fraction.ONE, new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
-        });
-        expected = new Equation(new Fraction[]{
-                Fraction.ONE, new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
-        });
-
-        // when
-        actual = EquationUtils.makeLeadingOne(equation);
-
-        // then
-        assertEquals(expected, actual);
+    private static Stream<Arguments> makeLeadingOneArgumentsProvider(){
+        return Stream.of(
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ONE, new Fraction(9, 8), new Fraction(-3, 14), Fraction.ZERO, new Fraction(5, 4)
+                        }),
+                        new Equation(new Fraction[]{
+                                new Fraction(2, 3), new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ONE, new Fraction(-4, 21), Fraction.ZERO, new Fraction(10, 9)
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ONE, Fraction.ZERO, new Fraction(-35, 6)
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ONE
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, new Fraction(5, 6)
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ONE
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ZERO, Fraction.ONE
+                        })
+                ),
+                Arguments.of(
+                        new Equation(new Fraction[]{
+                                Fraction.ONE, new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
+                        }),
+                        new Equation(new Fraction[]{
+                                Fraction.ONE, new Fraction(3, 4), new Fraction(-1, 7), Fraction.ZERO, new Fraction(5, 6)
+                        })
+                )
+        );
     }
 }
